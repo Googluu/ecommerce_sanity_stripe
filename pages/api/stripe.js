8cd7c851617604acde6a96ec598abdf68ae46a56
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-12-01 11:08:10 -0500

    18 step: stripe shopping options ++

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -4,6 +4,14 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_colletion: 'auto',
+        shipping_options: [
+          { shipping_rate: 'shr_1MAFBeArnQONanpDNSal5N54' },
+          { shipping_rate: 'shr_1MAFFLArnQONanpD4tMAOiRK' }
+        ],
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
