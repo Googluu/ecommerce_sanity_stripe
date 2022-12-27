95c947b77f3dd64cc01dbb9a7e2bf4dd1510b2dc
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-12-26 21:58:27 -0500

    19 step: pagina de compra exitosa ++

--- pages/success.js
+++ pages/success.js
@@ -6,8 +6,17 @@ import { useRouter } from 'next/router';
 import { useStateContext } from '../context/StateContext';
 
 const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+  const [order, setOrder] = useState(null);
+
   return (
-    <div>success</div>
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+      </div>
+    </div>
   )
 }
 
